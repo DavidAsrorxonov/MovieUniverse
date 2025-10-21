6800b0a669e817ae29fbb56590b39329883c7d63
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 20:20:25 +0900

    Initial Commit

---
+++ MovieUniverse/MovieUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieUniverseApp.swift
+//  MovieUniverse
+//
+//  Created by Dovudxon Asrorxonov on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
