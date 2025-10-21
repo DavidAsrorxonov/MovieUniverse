70240665e1da8576b2dce189ea973e13caf2a2da
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 20:28:45 +0900

    replace the hardcoded strings with constants

--- MovieUniverse/Constants.swift
+++ MovieUniverse/Constants.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+}
