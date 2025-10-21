4e8dfb797a8c6f6aaa6949f842453387adb14b03
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 20:31:20 +0900

    add constants for icons and use them

--- MovieUniverse/Constants.swift
+++ MovieUniverse/Constants.swift
@@ -12,4 +12,9 @@ struct Constants {
     static let upcomingString = "Upcoming"
     static let searchString = "Search"
     static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
 }
