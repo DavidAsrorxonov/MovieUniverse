7336044a860a3b83d3c670a990b4c32c4e28b0c0
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-23 18:09:16 +0900

    create a new view for trending movies; in constants, add trending movie string

---
+++ MovieUniverse/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalListView.swift
+//  MovieUniverse
+//
+//  Created by Dovudxon Asrorxonov on 23/10/25.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
