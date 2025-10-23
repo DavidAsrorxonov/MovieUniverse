f9f1c72711413db6ef303d5b5198d8b798b2c326
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-23 18:21:18 +0900

    add geometry reader to make the screen height 85% to prevent hero title jumping in when reloaded

--- MovieUniverse/HomeView.swift
+++ MovieUniverse/HomeView.swift
@@ -11,29 +11,32 @@ struct HomeView: View {
     var heroTestTitle = Constants.testTitleUrl
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                AsyncImage(url: URL(string: heroTestTitle)) {
-                    image in image.resizable().scaledToFit()
-                } placeholder: {
-                    ProgressView()
-                }
-                
-                HStack {
-                    Button {} label: {
-                        Text(Constants.playString)
-                            .ghostButton()
+        GeometryReader {geo in
+            ScrollView {
+                LazyVStack {
+                    AsyncImage(url: URL(string: heroTestTitle)) {
+                        image in image.resizable().scaledToFit()
+                    } placeholder: {
+                        ProgressView()
                     }
-                    Button {} label: {
-                        Text(Constants.downloadString)
-                            .ghostButton()
+                    .frame(width: geo.size.width, height: geo.size.height * 0.85)
+                    
+                    HStack {
+                        Button {} label: {
+                            Text(Constants.playString)
+                                .ghostButton()
+                        }
+                        Button {} label: {
+                            Text(Constants.downloadString)
+                                .ghostButton()
+                        }
                     }
+                    
+                    HorizontalListView(header: Constants.trendingMovieString)
+                    HorizontalListView(header: Constants.trendingTVString)
+                    HorizontalListView(header: Constants.topRatedMovieString)
+                    HorizontalListView(header: Constants.topRatedTVString)
                 }
-                
-                HorizontalListView(header: Constants.trendingMovieString)
-                HorizontalListView(header: Constants.trendingTVString)
-                HorizontalListView(header: Constants.topRatedMovieString)
-                HorizontalListView(header: Constants.topRatedTVString)
             }
         }
     }
