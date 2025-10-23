5b7833d987458ff2cf372a6ff6f895cedd26e9dc
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-23 18:02:41 +0900

    add more test title urls for testing purposes

--- MovieUniverse/Constants.swift
+++ MovieUniverse/Constants.swift
@@ -21,6 +21,8 @@ struct Constants {
     static let downloadIconString = "arrow.down.to.line"
     
     static let testTitleUrl = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
+    static let testTitleUrl2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    static let testTitleUrl3 = "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
 }
 
 extension Text {
